0edd5341c194d273824e9e59903d490281b83ad4
Will <will86325@gmail.com>
2023-05-08 08:30:30 -0400

    updates

--- libs/frontend/customer/feat-calendar/src/lib/Calendar/Calendar.tsx
+++ libs/frontend/customer/feat-calendar/src/lib/Calendar/Calendar.tsx
@@ -11,29 +11,28 @@ import {
 import 'react-big-calendar/lib/css/react-big-calendar.css';
 import { CalendarEvent } from './types';
 import { formatDateStripTime, plusOneHour } from './utils';
+import { useAppState } from '@remind-me/frontend/customer/util-store';
 
 const localizer = luxonLocalizer(DateTime);
 
-export function Calendar({
-  tasks,
-  initialDate,
-  onDateSelect,
-}: {
-  tasks: Task[];
-  initialDate: Date;
-  onDateSelect: (date: Date) => void;
-}) {
-  const [slotDateRange, setSlotDateRange] = useState<DateRange>([
-    initialDate,
-    DateTime.fromJSDate(initialDate)
+export function Calendar({ tasks }: { tasks: Task[] }) {
+  const [selectedDate, setSelectedDate, selectedTask, setSelectedTask] =
+    useAppState(
+      (state) =>
+        [
+          state.selectedDate,
+          state.setSelectedDate,
+          state.selectedTask,
+          state.setSelectedTask,
+        ] as const
+    );
+
+  const [slotEnd, setSlotEnd] = useState(
+    selectedDate
       .plus({
         hours: 1,
       })
-      .toJSDate(),
-  ]);
-
-  const [selectedEvent, setSelectedEvent] = useState<CalendarEvent | null>(
-    null
+      .toJSDate()
   );
 
   // const events: CalendarEvent[] = useMemo(
@@ -52,52 +51,59 @@ export function Calendar({
 
   const onNavigate = useCallback(
     (newDate: Date) => {
-      setSlotDateRange([newDate, plusOneHour(newDate)]);
-      onDateSelect(newDate);
+      const newDateTime = DateTime.fromJSDate(newDate);
+      setSelectedDate(newDateTime);
+      setSlotEnd(
+        newDateTime
+          .plus({
+            hours: 1,
+          })
+          .toJSDate()
+      );
     },
-    [setSlotDateRange, onDateSelect]
+    [setSlotEnd, setSelectedDate]
   );
 
   const onSelectEvent = useCallback(
     (event: CalendarEvent) => {
       console.log('event', event);
-      setSelectedEvent(event);
-      setSlotDateRange([event.start, event.end]);
+      setSelectedTask(event.task);
+      setSelectedDate(DateTime.fromJSDate(event.start));
+      setSlotEnd(event.end);
     },
-    [setSlotDateRange]
+    [setSelectedTask, setSelectedDate, setSlotEnd]
   );
 
-  const onSelectSlot = useCallback(
-    ({ slots }: SlotInfo) => {
-      if (slots.length >= 1) {
-        const slotStart = slots[0];
-        let slotEnd = plusOneHour(slotStart);
+  // const onSelectSlot = useCallback(
+  //   ({ slots }: SlotInfo) => {
+  //     if (slots.length >= 1) {
+  //       const slotStart = slots[0];
+  //       let slotEnd = plusOneHour(slotStart);
 
-        if (slots.length > 1) {
-          const lastDate = slots[slots.length - 1];
-          // make sure this occurs within the same day
-          if (
-            formatDateStripTime(slotStart) === formatDateStripTime(lastDate)
-          ) {
-            slotEnd = lastDate;
-          }
-        }
+  //       if (slots.length > 1) {
+  //         const lastDate = slots[slots.length - 1];
+  //         // make sure this occurs within the same day
+  //         if (
+  //           formatDateStripTime(slotStart) === formatDateStripTime(lastDate)
+  //         ) {
+  //           slotEnd = lastDate;
+  //         }
+  //       }
 
-        setSlotDateRange([slotStart, slotEnd]);
-        onDateSelect(slotStart);
-        addModalDisclosure.onOpen();
-      }
-    },
-    [addModalDisclosure, setSlotDateRange, onDateSelect]
-  );
+  //       setSlotEnd(slotEnd);
+  //       onDateSelect(slotStart);
+  //     }
+  //   },
+  //   [setSlotDateRange, onDateSelect]
+  // );
 
   return (
     <RCalendar
       selectable
       // events={events}
-      date={slotDateRange[0]}
+      date={selectedDate.toJSDate()}
       // onSelectEvent={onSelectEvent}
-      onSelectSlot={onSelectSlot}
+      // onSelectSlot={onSelectSlot}
       onNavigate={onNavigate}
       localizer={localizer}
       startAccessor="start"
